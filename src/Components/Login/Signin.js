32e74500d86904c21e06e5ca8106229e3c10f746
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-22 11:09:29 -0300

    signin page

--- src/Components/Login/Signin.js
+++ src/Components/Login/Signin.js
@@ -4,13 +4,22 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Signin() {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
     const [wait, setWait] = useState(false);
 
     function signin(e) {
         e.preventDefault();
         setWait(true);
+        if (password !== passwordConfirm) {
+            return setTimeout(() => {
+                alert('A senha e a confirmação devem ser iguais.');
+                setWait(false); 
+            }, 100);
+        }
+        setTimeout(() => setWait(false), 2000); //teste apenas
         //código de contato com o servidor e redirecionamento
     }
 
@@ -18,10 +27,12 @@ export default function Signin() {
         <Container>
             <Logo>MyWallet</Logo>
             <form onSubmit={e => signin(e)}>
+                <Input disabled={wait} required type="text" placeholder="Nome" onChange={e => setName(e.target.value)} value={name}/>
                 <Input disabled={wait} required type="email" placeholder="E-mail" onChange={e => setEmail(e.target.value)} value={email}/>
                 <Input disabled={wait} required type="password" placeholder="Senha" onChange={e => setPassword(e.target.value)} value={password}/>
+                <Input disabled={wait} required type="password" placeholder="Confirme a senha" onChange={e => setPasswordConfirm(e.target.value)} value={passwordConfirm}/>
                 <Button disabled={wait}>
-                    Entrar
+                    Cadastrar
                 </Button>
             </form>
             <Link to="/login">Já tem uma conta? Entre agora!</Link>
